845469818580a9bcc7ee38c64fbfb79ca58d89e0
Guilherme Paiva <48967900+guipaiva@users.noreply.github.com>
2019-06-16 17:28:37 -0300

    Delete loader.py

--- loader.py
+++
@@ -1,9 +0,0 @@
-import pickle 
-
-#carrega o arquivo binário criado e atribui às variáveis
-with open("dump.pkl","br") as dmp:
-	data = pickle.load(dmp)
-x = data[0]
-labels = data[1]
-norms = data[2]
-
